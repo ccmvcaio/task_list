7f6c85c166c4797ea96ecc5836eea8dec321e206
Caio César de Morais Valério <ccmvcaio@gmail.com>
2020-07-23 20:58:00 -0300

    CRUD completo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,4 +18,10 @@ class TasksController < ApplicationController
         redirect_to root_path
     end
 
+    def destroy
+        task = Task.find(params[:id])
+        task.destroy
+        redirect_to root_path
+    end
+
 end
